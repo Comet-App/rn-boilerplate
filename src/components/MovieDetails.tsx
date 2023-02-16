ba7d775901f6b2c637ad38a6d3c84e70eaf66617
Ajay M <ajay@shivy.co.in>
2023-02-17 00:43:02 +0530

    Making api call on component mount

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,13 +1,42 @@
 // Functional react component blueprint
+import {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
+import {console_log} from '~/utils/helper';
+import axios from '~/utils/axios';
 
 const MovieDetails = () => {
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console_log('Fetching movie details');
+    setLoading(true);
+
+    fetchMovieDetails().then(movie => {
+      setMovie(movie);
+    });
+    // Fetch movie details
+    setLoading(false);
+  }, []);
+
+  // API function to fetch movie details
+  const fetchMovieDetails = async () => {
+    const response = await axios.get('/');
+    console_log(response.data);
+    // Fetch movie details
+    return {
+      ...response.data,
+    };
+  };
+
   return (
     <View>
+      {loading && <Text>Loading...</Text>}
       <Text>Movie Details</Text>
     </View>
   );
 };
+
 // Need to create a variable and then export it. Instead of doing something like:
 // export default () => {
 //   return (
